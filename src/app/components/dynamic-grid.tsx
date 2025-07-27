f0e0e01c8c357c6a01ee57b35f47ea201e803afd
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-07-27 20:07:29 +0200

    Add dynamic grid

--- src/app/components/dynamic-grid.tsx
+++ src/app/components/dynamic-grid.tsx
@@ -1,24 +1,58 @@
-import GridLayout from 'react-grid-layout';
+'use client';
 import { Responsive as ResponsiveGridLayout } from 'react-grid-layout';
 
 export default function DynamicGrid() {
-  // layout is an array of objects, see the demo for more complete usage
-  const layout = [
-    { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: 'c', x: 4, y: 0, w: 1, h: 2 },
-  ];
+  const layouts = {
+    lg: [
+      { i: 'a', x: 0, y: 0, w: 4, h: 2 },
+      { i: 'b', x: 4, y: 0, w: 4, h: 2 },
+      { i: 'c', x: 8, y: 0, w: 4, h: 2 },
+    ],
+    md: [
+      { i: 'a', x: 0, y: 0, w: 3, h: 2 },
+      { i: 'b', x: 3, y: 0, w: 3, h: 2 },
+      { i: 'c', x: 6, y: 0, w: 3, h: 2 },
+    ],
+    sm: [
+      { i: 'a', x: 0, y: 0, w: 2, h: 2 },
+      { i: 'b', x: 2, y: 0, w: 2, h: 2 },
+      { i: 'c', x: 4, y: 0, w: 2, h: 2 },
+    ],
+    xs: [
+      { i: 'a', x: 0, y: 0, w: 1, h: 2 },
+      { i: 'b', x: 1, y: 0, w: 1, h: 2 },
+      { i: 'c', x: 2, y: 0, w: 1, h: 2 },
+    ],
+    xxs: [
+      { i: 'a', x: 0, y: 0, w: 1, h: 1 },
+      { i: 'b', x: 1, y: 0, w: 1, h: 1 },
+      { i: 'c', x: 2, y: 0, w: 1, h: 1 },
+    ],
+  };
+
+  const layout = {
+    lg: [...layouts.lg],
+    md: [...layouts.md],
+    sm: [...layouts.sm],
+    xs: [...layouts.xs],
+    xxs: [...layouts.xxs],
+  };
   return (
-    <GridLayout
+    <ResponsiveGridLayout
       className="layout"
-      layout={layout}
-      cols={12}
-      rowHeight={30}
-      width={1200}
+      layouts={layouts}
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
     >
-      <div key="a">a</div>
-      <div key="b">b</div>
-      <div key="c">c</div>
-    </GridLayout>
+      <div className="glass-card" key="1">
+        1
+      </div>
+      <div className="glass-card" key="2">
+        2
+      </div>
+      <div className="glass-card" key="3">
+        3
+      </div>
+    </ResponsiveGridLayout>
   );
 }
