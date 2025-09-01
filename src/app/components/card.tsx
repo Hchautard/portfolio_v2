532847cf06a6b6115519762fd5d04bd80d6a2f75
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-09-01 21:35:04 +0200

    Change display grid method.

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,4 +1,6 @@
-export default function Card({ children = '', className = '' }) {
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+export default function Card({ children, className = '' }) {
   return (
     <div
       className={`glass flex flex-col items-center justify-center p-4 rounded-lg shadow-lg ${className}`}
--- src/app/components/dynamic-grid.tsx
+++ src/app/components/dynamic-grid.tsx
@@ -1,59 +1,21 @@
 'use client';
-import { Responsive as ResponsiveGridLayout } from 'react-grid-layout';
 import Card from '@/app/components/card';
+import '../styles/dynamic-grid.css';
 
 export default function DynamicGrid() {
-  const layouts = {
-    lg: [
-      { i: 'a', x: 0, y: 0, w: 4, h: 2 },
-      { i: 'b', x: 4, y: 0, w: 4, h: 2 },
-      { i: 'c', x: 8, y: 0, w: 4, h: 2 },
-    ],
-    md: [
-      { i: 'a', x: 0, y: 0, w: 3, h: 2 },
-      { i: 'b', x: 3, y: 0, w: 3, h: 2 },
-      { i: 'c', x: 6, y: 0, w: 3, h: 2 },
-    ],
-    sm: [
-      { i: 'a', x: 0, y: 0, w: 2, h: 2 },
-      { i: 'b', x: 2, y: 0, w: 2, h: 2 },
-      { i: 'c', x: 4, y: 0, w: 2, h: 2 },
-    ],
-    xs: [
-      { i: 'a', x: 0, y: 0, w: 1, h: 2 },
-      { i: 'b', x: 1, y: 0, w: 1, h: 2 },
-      { i: 'c', x: 2, y: 0, w: 1, h: 2 },
-    ],
-    xxs: [
-      { i: 'a', x: 0, y: 0, w: 1, h: 1 },
-      { i: 'b', x: 1, y: 0, w: 1, h: 1 },
-      { i: 'c', x: 2, y: 0, w: 1, h: 1 },
-    ],
-  };
-
-  const layout = {
-    lg: [...layouts.lg],
-    md: [...layouts.md],
-    sm: [...layouts.sm],
-    xs: [...layouts.xs],
-    xxs: [...layouts.xxs],
-  };
   return (
-    <ResponsiveGridLayout
-      className="layout flex items-center justify-center gap-4"
-      layouts={layout}
-      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-      cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-    >
-      <Card className="glass-card" key="a">
-        This is a Card
-      </Card>
-      <Card className="glass-card" key="b">
-        This is a Card
-      </Card>
-      <Card className="glass-card" key="c">
-        This is a Card
-      </Card>
-    </ResponsiveGridLayout>
+    <div className="wrapper">
+      <Card className="glass-card one">This is a Card</Card>
+      <div className="glass-card two">
+        <Card className="two-one">Nested Card</Card>
+
+        <Card className="two-two">This is a Card</Card>
+      </div>
+      <Card className="glass-card three">This is a Card</Card>
+
+      <Card className="glass-card four">This is a Card</Card>
+      <Card className="glass-card five">This is a Card</Card>
+      <Card className="glass-card six">This is a Card</Card>
+    </div>
   );
 }
