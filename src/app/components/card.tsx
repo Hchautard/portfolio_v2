fee3df8af5170c41989777ed1f97c64880baeee6
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-09-03 22:10:18 +0200

    Add navy sound effect on project card.

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,9 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-export default function Card({ children, className = '' }) {
+import React from 'react';
+
+export default function Card({
+  children,
+  className = '',
+  onMouseEnter,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  onMouseEnter?: () => void;
+}) {
+  function handleMouseEnter() {
+    if (onMouseEnter) {
+      onMouseEnter();
+    }
+  }
+
   return (
     <div
       className={`glass flex flex-col items-center justify-center p-4 rounded-lg shadow-lg ${className} hover:scale-[1.03] transition-transform duration-300`}
+      onMouseEnter={handleMouseEnter}
     >
       {children}
     </div>
--- src/app/components/project-dynamic-grid.tsx
+++ src/app/components/project-dynamic-grid.tsx
@@ -1,13 +1,45 @@
 'use client';
+import { useCallback, useRef } from 'react';
 import Card from '@/app/components/card';
 import '../styles/project-dynamic-grid.css';
 
+export const useSound = (soundPath: string, throttleTime = 5000) => {
+  const audioRef = useRef(null as HTMLAudioElement | null);
+  const lastPlayedRef = useRef(0);
+
+  return useCallback(() => {
+    const now = Date.now();
+
+    if (now - lastPlayedRef.current < throttleTime) {
+      return;
+    }
+
+    lastPlayedRef.current = now;
+
+    if (!audioRef.current) {
+      audioRef.current = new Audio(soundPath);
+      audioRef.current.volume = 1;
+    }
+
+    audioRef.current.currentTime = 0;
+    audioRef.current.play().catch(console.error);
+  }, [soundPath, throttleTime]);
+};
+
 export default function ProjectDynamicGrid() {
+  const playSound = useSound('assets/sounds/hey_listen_navy.mp3', 4000);
+
   return (
     <div className="project-wrapper">
-      <Card className="glass-card one">This is a Card</Card>
-      <Card className="glass-card one">This is a Card</Card>
-      <Card className="glass-card one">This is a Card</Card>
+      <Card onMouseEnter={playSound} className="glass-card one">
+        This is a Card
+      </Card>
+      <Card onMouseEnter={playSound} className="glass-card one">
+        This is a Card
+      </Card>
+      <Card onMouseEnter={playSound} className="glass-card one">
+        This is a Card
+      </Card>
 
       <Card className="glass-card one">This is a Card</Card>
       <Card className="glass-card one">This is a Card</Card>
