f3e8d58bb75b60de3c8dcb13a96cfb7d08915044
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-02-03 11:30:12 +0100

    Update construction status; set isUnderConstruction to false to display Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Home from '../app/pages/home';
 import UnderConstruction from '@/app/pages/under-construction';
 
 export default function App() {
-  const isUnderConstruction = true;
+  const isUnderConstruction = false;
 
   return isUnderConstruction ? <UnderConstruction /> : <Home />;
 }
